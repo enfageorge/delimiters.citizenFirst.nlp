d3a413021d9f6023f6c6124cc16d497943b3586d
Enfa Rose George <enfa.rose@turbolab.in>
2019-11-09 15:03:17 +0530

    function to show keywords added

---
+++ backendScripting/keywordDetail.py
@@ -0,0 +1,6 @@
+import csv
+def viewKeywords():
+    with open('data/causekeywords.csv', 'r') as f:
+        reader = csv.reader(f)
+        your_list = list(reader)
+    return [keyword[0] for keyword in your_list]
