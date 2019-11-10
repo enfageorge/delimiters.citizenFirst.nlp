8de437e7f1b6cab4adc3947c002befd36ad4d313
Enfa Rose George <enfa.rose@turbolab.in>
2019-11-10 07:30:05 +0530

    newsfeed for ministers implemented

--- backendScripting/ministers.py
+++ backendScripting/ministers.py
@@ -1,8 +1,24 @@
 import pandas as pd
 import ast
+
+def viewNewsArticles(newsIDs,count = 50):
+    df = pd.read_csv('data/politics18.csv')
+    newsfeed = []
+    renderedCount = 0
+    matcheddf = df[df.index.isin(newsIDs)]
+    for row in matcheddf.itertuples():
+                    tagged = ast.literal_eval(row[4]) 
+                    taggedMinisters = str(row[8]).split(',')
+                    newsfeed.append({'author' : row[1],'content' : row[2],'date' : row[3],'tags' :tagged ,'title' : row[5],'url' : row[6],'website' : row[7],'taggedMinisters' :taggedMinisters })
+                    renderedCount += 1
+                    newsMatchedforTopic = 1
+                    if newsMatchedforTopic >=count:
+                        break
+    return newsfeed
+
 def returnDetails(ministerName):
-    #ministerName = 'Narendra Modi'
-    row = checkDf.loc[df['Name'] == ministerName]
+    df =  pd.read_csv('data/ministers.csv')
+    row = df.loc[df['Name'] == ministerName]
     name = ministerName
     party = row['Party'].to_list()[0]
     portfolio =  row['Portfolio'].to_list()[0] #Fix Portfolio Later
@@ -12,7 +28,7 @@ def returnDetails(ministerName):
     details = {'name' : name,'party': party,
                'portfolio' : portfolio, 
                'terms' : terms,
-               'newsIDs' : newsIDs}
+               'newsfeed' : viewNewsArticles(newsIDs)}
     
     #Fix Portfolio Later
     return details
